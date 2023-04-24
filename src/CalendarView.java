01e8be7d29c0d970a98a582772a44d6811d13d63
mikemulla22 <michael.preys@menloschool.org>
2023-04-24 09:56:20 -0700

    The Start!

---
+++ src/CalendarView.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class CalendarView extends JFrame {
+    private final int WINDOW_WIDTH = 1000;
+    private final int WINDOW_HEIGHT = 800;
+    private Calendar c;
+
+    public CalendarView(Calendar c) {
+        this.c = c;
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Calendar 2023");
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setVisible(true);
+    }
+}
