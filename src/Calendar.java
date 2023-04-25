c1a088f8cf76b65bf99d3341a47e4f5f6d82fce9
mikemulla22 <michael.preys@menloschool.org>
2023-04-24 17:51:07 -0700

    Function Headers and instance variables.

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
 public class Calendar {
+    private int year;
+    private ArrayList<Month> calendar;
+    private CalendarView window;
+
+    public Calendar() {
+        this.window = new CalendarView(this);
+        this.year = 2023;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
 
 }
--- src/Day.java
+++ src/Day.java
@@ -1,3 +1,26 @@
 public class Day {
+    private int numDay;
+    private int numApts;
+    private String apts;
+
+    public Day(int numDay) {
+        this.numDay = numDay;
+    }
+
+    public int getNumDay() {
+        return this.numDay;
+    }
+
+    public int getNumApts() {
+        return this.numApts;
+    }
+
+    public String getApts() {
+        return this.apts;
+    }
+
+    public void setApts(String apt) {
+        this.apts = this.apts + ", " + apt;
+    }
 
 }
