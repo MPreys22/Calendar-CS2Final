8b08918646414cd8f895c0425b80a4aad8616c93
mikemulla22 <michael.preys@menloschool.org>
2023-04-24 17:51:16 -0700

    Function Headers and instance variables.

--- src/CalendarView.java
+++ src/CalendarView.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class CalendarView extends JFrame {
+public class CalendarView extends JFrame implements KeyListener {
     private final int WINDOW_WIDTH = 1000;
     private final int WINDOW_HEIGHT = 800;
     private Calendar c;
@@ -12,8 +14,25 @@ public class CalendarView extends JFrame {
         this.setTitle("Calendar 2023");
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         this.setVisible(true);
+        addKeyListener(this);
     }
-
     public void paint(Graphics g) {
     }
+
+    public void keyTyped(KeyEvent e)
+    {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+
+    }
+
 }
