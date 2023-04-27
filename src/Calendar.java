ff57429f279092759e10e0d5663586f10c3aa253
mikemulla22 <michael.preys@menloschool.org>
2023-04-26 22:09:43 -0700

    Function Headers and instance variables. Start of function: Prototype

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,16 +1,48 @@
 import java.util.ArrayList;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 public class Calendar {
     private int year;
     private ArrayList<Month> calendar;
+    private String[] months;
     private CalendarView window;
+    private Image mainCalendar;
 
     public Calendar() {
-        this.window = new CalendarView(this);
         this.year = 2023;
+        calendar = new ArrayList<>();
+        months = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        mainCalendar = new ImageIcon("Resources/FullYear.png").getImage();
+        this.window = new CalendarView(this);
+        for(int i=0; i<12; i++) {
+            if(oneEquals(i, 0, 2, 4, 6, 7, 9, 11)) {
+                calendar.add(new Month(months[i], 31));
+            }
+            else if (i == 1){
+                calendar.add(new Month(months[i], 28));
+            }
+            else {
+                calendar.add(new Month(months[i], 30));
+            }
+        }
+    }
+
+    public boolean oneEquals(int start, int m0, int m1, int m2, int m3, int m4, int m5, int m6){
+        if(start == m0 || start == m1 || start == m2 || start == m3 || start == m4 || start == m5 || start == m6) {
+            return true;
+        }
+        return false;
     }
 
     public int getYear() {
         return this.year;
     }
 
+    public Image getMainCalendar() {
+        return mainCalendar;
+    }
+
+    public static void main(String[] args) {
+        Calendar newYear = new Calendar();
+    }
 }
--- src/CalendarView.java
+++ src/CalendarView.java
@@ -1,22 +1,35 @@
 import javax.swing.*;
-import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.*;
+import java.awt.event.*;
 
-public class CalendarView extends JFrame implements KeyListener {
-    private final int WINDOW_WIDTH = 1000;
-    private final int WINDOW_HEIGHT = 800;
+public class CalendarView extends JFrame implements KeyListener, MouseListener, MouseMotionListener {
+    private final int WINDOW_WIDTH = 800;
+    private final int WINDOW_HEIGHT = 825;
     private Calendar c;
+    private String month;
+    private int day;
 
     public CalendarView(Calendar c) {
+        month = "";
         this.c = c;
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Calendar 2023");
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        this.setVisible(true);
         addKeyListener(this);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        this.setVisible(true);
+
     }
     public void paint(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(0,0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        if(month.equals("J")) {
+            g.drawString("Gello", 100, 100);
+        }
+        else {
+            g.drawImage(c.getMainCalendar(), 0, 25, 800, 800, this);
+        }
     }
 
     public void keyTyped(KeyEvent e)
@@ -35,4 +48,46 @@ public class CalendarView extends JFrame implements KeyListener {
 
     }
 
+    public void mousePressed(MouseEvent e)
+    {
+        // Make the circle green whenever you press the mouse.
+        if((e.getX() > 0 && e.getX() < 247) && (e.getX() > 0 && e.getX() < 204)) {
+            month = "J";
+            repaint();
+        }
+
+    }
+
+
+    public void mouseReleased(MouseEvent e)
+    {
+        // Make the circle blue whenever you let go of the mouse
+
+    }
+
+
+    public void mouseClicked(MouseEvent e)
+    {
+
+    }
+
+
+    public void mouseEntered(MouseEvent e)
+    {
+
+    }
+
+    public void mouseExited(MouseEvent e)
+    {
+
+    }
+
+    public void mouseDragged(MouseEvent e) { // this method gets called every time you click and drag
+        int xMouse = e.getX();
+        int yMouse = e.getY();
+    }
+
+    public void mouseMoved(MouseEvent e) { // this method gets called everytime you move the mouse
+
+    }
 }
